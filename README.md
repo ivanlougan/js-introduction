4402875bb99d97e458aeeba408c028c134e85ad1
antariess <velislava.n.georgieva@gmail.com>
2024-04-02 14:52:50 +0100

    add exports tasks

---
+++ README.md
@@ -0,0 +1,3 @@
+# Of Files and Functions
+
+For instructions, please visit the relevant lesson on Learn 2 Code: 
---
+++ modules-and-exports/connect-the-files/array/data/data.js
@@ -0,0 +1,5 @@
+// do not change the code int his file
+
+const numbers = [3, 9, 462]
+
+module.exports = numbers
